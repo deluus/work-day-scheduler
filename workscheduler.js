134dc5571e4e533de4b225214c4c692a8087752c
deluus polycarp <pdeluus@gmail.com>
2021-12-03 13:53:14 -0800

    hw5

--- workscheduler.js
+++ workscheduler.js
@@ -1,7 +1,7 @@
 var currentTimeEl = $("#current-time");
-var timeBlockEl = $('.container') ;
-var currentHrEl = "";
-var textEL =
+var container = $('.container') ;
+var timeBlock =$('.time-block');
+
 
    
 
@@ -12,12 +12,44 @@ setInterval (function() {
 }, 1000);
 // console.log(currentTimeEl)
 
+var meridiem = "";
+if (i <11 ){
+    meridiem = i + "AM";
+}
+else {
+    meridiem = i + "PM"
+};
+console.log(meridiem)
+
+
 
 
 // for loop to include all the hrs
 for (var i=9; i <= 17; i ++){
     // console.log(i)
 
-}
-    var savedValueHr = localStorage.getItem("hour " + i);
 
+
+
+    var savedValueHr = localStorage.getItem("hour " + i);
+    // console.log(savedValueHr)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+//  creating an function that listens for any clicks with in the container. 
+$('.container').on('click','button', function(event){
+
+    // access the curent event 
+    event.target
+
+});
