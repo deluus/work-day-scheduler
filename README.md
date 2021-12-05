0f0945f414b30440008ac4338d014fdb73ce5eb3
deluus polycarp <pdeluus@gmail.com>
2021-12-04 17:51:08 -0800

    hw5

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # work-scheduler
 
 # what will my application need
-  - 
+  - current date and time
+    - current hour
+    - compare with calendar hours
+
+ # what data will the application need to interact with?
+ - create text area
+ - ability to input text on time blocks
+ - text saves to local storage , allows to show after page is refreshed
+ - time blocks are color coded to show past, present, future
+
+
+    
--- workscheduler.js
+++ workscheduler.js
@@ -12,7 +12,9 @@ setInterval (function() {
 
 
     // for loop to include all the hrs
-for (var i=9; i <= 17; i ++){    
+    
+for (var i=5; i <= 23; i ++){    
+    // creating the elements and inserting
     var timeBlock = $('<div>').attr('id', 'hour-'+i).attr('class', 'row time-block')
     var span = $('<span>')
     var timeHour = $('<div>').attr('class', 'col-md-1 hour').text(moment(i.toString(), 'k kk').format('h'))
@@ -59,9 +61,26 @@ function checkHour() {
     })
 }
 checkHour();
-
+$('#hour-5 .description').val(localStorage.getItem("hour-5"))
+$('#hour-6 .description').val(localStorage.getItem("hour-6"))
+$('#hour-7 .description').val(localStorage.getItem("hour-7"))
+$('#hour-8 .description').val(localStorage.getItem("hour-8"))
 $('#hour-9 .description').val(localStorage.getItem("hour-9"))
-
+$('#hour-10 .description').val(localStorage.getItem("hour-10"))
+$('#hour-11 .description').val(localStorage.getItem("hour-11"))
+$('#hour-12 .description').val(localStorage.getItem("hour-12"))
+$('#hour-13 .description').val(localStorage.getItem("hour-13"))
+$('#hour-14 .description').val(localStorage.getItem("hour-13"))
+$('#hour-14 .description').val(localStorage.getItem("hour-14"))
+$('#hour-15 .description').val(localStorage.getItem("hour-15"))
+$('#hour-16 .description').val(localStorage.getItem("hour-16"))
+$('#hour-17 .description').val(localStorage.getItem("hour-17"))
+$('#hour-18 .description').val(localStorage.getItem("hour-18"))
+$('#hour-19 .description').val(localStorage.getItem("hour-19"))
+$('#hour-20 .description').val(localStorage.getItem("hour-20"))
+$('#hour-21 .description').val(localStorage.getItem("hour-21"))
+$('#hour-22 .description').val(localStorage.getItem("hour-22"))
+$('#hour-23 .description').val(localStorage.getItem("hour-23"))
 
 
 
